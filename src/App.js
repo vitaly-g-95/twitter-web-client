a4ce8df07338ed386652fc5aa640922d1cb08b75
Vitaly Gumenyak <vetalgr@gmail.com>
2020-01-28 23:15:33 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import routes from './routes';
+import Navbar from './components/Navbar';
 import InlineForm from './components/InlineForm';
 
 function App() {
   return (
     <Router>
       <div className="container py-5">
-        <InlineForm
-          inputName="q"
-          buttonText="Search"
-        />
+        <Navbar>
+          <InlineForm
+            inputName="q"
+            buttonText="Search"
+          />
+        </Navbar>
 
         <Suspense fallback={<div>Loading</div>}>
           <Switch>
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import routes from '../routes';
+
+const Navbar = ({ children }) => (
+  <nav className="navbar navbar-expand navbar-light bg-light mb-4">
+    <ul className="navbar-nav mr-auto">
+      {routes.map(({ exact, path, name }) => (
+        <li
+          key={path}
+          className="nav-item"
+        >
+          <NavLink
+            exact={exact}
+            to={path}
+            className="nav-link"
+            activeClassName="active"
+          >
+            {name}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+
+    <div className="w-auto">
+      {children}
+    </div>
+  </nav>
+);
+
+export default Navbar;
