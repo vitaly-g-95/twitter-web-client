fc025a716eb66b15bf5d618cfd39e3d13b1981c6
Vitaly Gumenyak <vetalgr@gmail.com>
2020-01-28 22:41:41 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import routes from './routes';
 import InlineForm from './components/InlineForm';
 
 function App() {
   return (
-    <div className="container py-5">
-      <InlineForm
-        inputName="q"
-        buttonText="Search"
-      />
-    </div>
+    <Router>
+      <div className="container py-5">
+        <InlineForm
+          inputName="q"
+          buttonText="Search"
+        />
+
+        <Suspense fallback={<div>Loading</div>}>
+          <Switch>
+            {routes.map(({ exact, path, component }) => (
+              <Route
+                key={path}
+                exact={exact}
+                path={path}
+                component={lazy(() => import(`./pages/${component}`))}
+              />
+            ))}
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const routes = [
+  {
+    exact: true,
+    path: '/',
+    component: 'Home',
+    name: 'Home'
+  },
+  {
+    path: '/search',
+    component: 'Search',
+    name: 'Search'
+  }
+];
+
+export default routes;
