aded78c651ca1bce4c8545adf38f38c472dd6930
Vitaly Gumenyak <vetalgr@gmail.com>
2020-01-29 01:33:20 +0200

    change title

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 
 const Page = ({ title, children }) => (
   <div className="px-3">
-    <h1 className="h4">
-      {title}
-    </h1>
+    <h1
+      className="h4"
+      dangerouslySetInnerHTML={{
+        __html: title
+      }}
+    />
 
     {children}
   </div>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import queryString from 'query-string';
 import Page from './Page';
 
-const Search = () => {
+const Search = ({ location }) => {
+  const { q } = queryString.parse(location.search);
+  
+  const title = q.length > 2
+    ? `Search results for &laquo;${q}&raquo;`
+    : 'Type your search query';
+
   return (
-    <Page title="Search">
+    <Page title={title}>
       
     </Page>
   );
 };
 
-export default Search;
+export default withRouter(Search);
