992beeadf731ac5d7f385e1925c63d407a2241cb
Vitaly Gumenyak <vetalgr@gmail.com>
2020-01-28 21:06:08 +0200

    add input value control

--- src/components/InlineForm.js
+++ src/components/InlineForm.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const InlineForm = ({ inputName, buttonText, onSubmit }) => (
-  <form className="input-group" onSubmit={onSubmit}>
-    <input
-      type="text"
-      className="form-control"
-      name={inputName}
-    />
-    <div className="input-group-append">
-      <button className="btn btn-primary" type="submit">
-        {buttonText}
-      </button>
-    </div>
-  </form>
-);
+const InlineForm = ({ inputName, buttonText, onSubmit }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
+  const _onSubmit = e => {
+    e.preventDefault();
+
+    onSubmit(value);
+  };
+
+  return (
+    <form className="input-group" onSubmit={_onSubmit}>
+      <input
+        type="text"
+        className="form-control"
+        name={inputName}
+        onChange={onChange}
+      />
+      <div className="input-group-append">
+        <button className="btn btn-primary" type="submit">
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
+};
 
 InlineForm.propTypes = {
   inputName: PropTypes.string.isRequired,
