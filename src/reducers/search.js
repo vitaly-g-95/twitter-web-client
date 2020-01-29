36d723bb07cc8aad18775bcb051038f4e845d809
Vitaly Gumenyak <vetalgr@gmail.com>
2020-01-29 03:39:11 +0200

    add search reducer

---
+++ src/reducers/search.js
@@ -0,0 +1,13 @@
+const initialState = {
+  query: ''
+};
+
+const searchReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+  }
+
+  return state;
+};
+
+export default searchReducer;
